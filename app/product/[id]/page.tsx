8832d56efb3f894c71aceaa0af4589c86cfd2484
Rupesh <rkshandilya001@gmail.com>
2025-05-24 00:21:22 +0530

    Resolving deployment error

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,10 +1,13 @@
 import ProductDetail from '@/components/ProductDetail';
 import { products } from '@/data/product';
-import { use } from 'react';
 
-export default function ProductPage({ params }:  { params: Promise<{ id: string }> }) {
-    const { id } = use(params);
-  const product = products.find(p => p.id === parseInt(id));
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
+export default function ProductPage({ params }: PageProps) {
+  const product = products.find(p => p.id === parseInt(params.id));
   return <ProductDetail product={product} />;
 }
